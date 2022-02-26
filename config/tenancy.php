f186801f8ea09235d64ee9049b3df749c1c32c45
Mario <maritojhefi@outlook.com>
2022-02-25 22:19:52 -0400

    lo que sea

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,7 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'appbuilder2.test'
+        'busco-guru.com'
     ],
 
     /**
